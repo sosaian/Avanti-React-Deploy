14a390647d4148ca5ab89fd476267f0ce1e08f05
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-23 17:27:04 -0300

    Add all frontend changes up to commit e812b27c7de79529bf82ed3ae7b78f4acf30659a
    Remember to check this commit in official Avanti repository
    
    https://github.com/No-Country-simulation/S20-17-webapp/commit/e812b27c7de79529bf82ed3ae7b78f4acf30659a

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { LandingPage, UserLoginPage, UserRegisterPage } from "./pages"
+import { LandingPage, UserLoginPage, UserRegisterPage, WIPPage } from "./pages"
 
 function App() {
-    const WIP_MESSAGE = "Página aún en construcción..."
     const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/search" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/proyect/:proyectId" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/checkout/:proyectId" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/search" element={<WIPPage />} />
+                <Route path="/proyect/:proyectId" element={<WIPPage />} />
+                <Route path="/checkout/:proyectId" element={<WIPPage />} />
                 <Route path="/login" element={<UserLoginPage />} />
                 <Route path="/register" element={<UserRegisterPage />} />
-                <Route path="/about" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/recovery" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/mission" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/contact" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/tips" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/process" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/about" element={<WIPPage />} />
+                <Route path="/recovery" element={<WIPPage />} />
+                <Route path="/mission" element={<WIPPage />} />
+                <Route path="/contact" element={<WIPPage />} />
+                <Route path="/tips" element={<WIPPage />} />
+                <Route path="/process" element={<WIPPage />} />
                 <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
             </Routes>
         </BrowserRouter>
---
+++ src/pages/WIPPage/WIPPage.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+export function WIPPage() {
+    return (
+        <div className="grid h-screen w-full grid-rows-[1fr_8fr_1fr] items-center bg-black text-white">
+            <header className="flex items-center justify-center border-b-2 border-b-avanti-light-green py-4">
+                <h1>AVANTI</h1>
+            </header>
+            <main className="grid place-items-center">
+                <div className="m-4 flex flex-col gap-4 rounded-6xl border-2 border-solid border-avanti-light-green p-8">
+                    <h2 className="">Página aún en construcción...</h2>
+                    <p>Intenta más tarde, ¡quizás mañana ya esté listo para usar!</p>
+                    <button
+                        className="grid min-h-12 min-w-12 place-items-center rounded-2xl bg-white bg-gradient-avanti bg-[length:200%_200%] bg-left text-white shadow-md transition-[background-position] duration-500 ease-in-out hover:bg-right"
+                        tabIndex={-1}
+                    >
+                        <Link
+                            to="/"
+                            className="grid h-full w-full place-items-center rounded-2xl focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
+                            tabIndex={0}
+                        >
+                            {" "}
+                            ¡Entendido!
+                        </Link>
+                    </button>
+                </div>
+            </main>
+            <footer className="flex items-center justify-center border-t-2 border-t-avanti-light-green py-4">
+                <h2>NoCountry | Equipo-s20-17-webapp</h2>
+            </footer>
+        </div>
+    )
+}
